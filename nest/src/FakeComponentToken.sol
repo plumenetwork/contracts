91fbb310c9eebd4054c23145541811878a7c21fe
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-09-13 04:52:29 -0700

    forge fmt

--- nest/src/FakeComponentToken.sol
+++ nest/src/FakeComponentToken.sol
@@ -16,7 +16,13 @@ import { IComponentToken } from "./interfaces/IComponentToken.sol";
  * Users can buy and sell one FakeComponentToken by exchanging it with one CurrencyToken at any time.
  * @custom:oz-upgrades-from FakeComponentToken
  */
-contract FakeComponentToken is Initializable, AccessControlUpgradeable, UUPSUpgradeable, ERC20Upgradeable, IComponentToken {
+contract FakeComponentToken is
+    Initializable,
+    AccessControlUpgradeable,
+    UUPSUpgradeable,
+    ERC20Upgradeable,
+    IComponentToken
+{
 
     // Storage
 
