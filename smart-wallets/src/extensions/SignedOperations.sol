0e8c87ac8924db4f5c62cabffe8fc64e5893db40
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-09-16 05:51:47 -0700

    unpin versions

--- smart-wallets/src/extensions/SignedOperations.sol
+++ smart-wallets/src/extensions/SignedOperations.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity ^0.8.25;
 
 import { IERC1271 } from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
