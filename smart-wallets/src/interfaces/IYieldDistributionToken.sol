053d6498acba28aeaa5be8d3c681a54d9cafb0c7
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-09-16 23:22:39 -0700

    update comments, errors, and events

--- smart-wallets/src/interfaces/IYieldDistributionToken.sol
+++ smart-wallets/src/interfaces/IYieldDistributionToken.sol
@@ -1,21 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity ^0.8.25;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-/**
- * @title IYieldDistributionToken
- * @author Eugene Y. Q. Shen
- * @notice Interface for the Yield Distribution
- */
 interface IYieldDistributionToken is IERC20 {
 
-    /**
-     * @notice Claim yield for the token holder
-     * @dev sender is the token holder
-     * @return currency The token that was yielded
-     * @return amount The amount of tokens that was yielded
-     */
     function claimYield() external returns (address currency, uint256 amount);
+    function processYield(address user) external;
+    function _depositYield(uint256 timestamp, uint256 amount) internal;
 
 }
