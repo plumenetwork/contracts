500b896b6f1bcfe72990f5aced7fb95d29f3472b
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-09-20 08:06:48 -0700

    [NES-211] resolve two bugs with checking balances and limiting list sizes (#23)

--- smart-wallets/src/interfaces/IAssetVault.sol
+++ smart-wallets/src/interfaces/IAssetVault.sol
@@ -1,26 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import { AssetToken } from "../token/AssetToken.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { IAssetToken } from "../interfaces/IAssetToken.sol";
 
 interface IAssetVault {
 
     function updateYieldAllowance(
-        AssetToken assetToken,
+        IAssetToken assetToken,
         address beneficiary,
         uint256 amount,
         uint256 expiration
     ) external;
-    function redistributeYield(AssetToken assetToken, ERC20 currencyToken, uint256 currencyTokenAmount) external;
+    function redistributeYield(IAssetToken assetToken, IERC20 currencyToken, uint256 currencyTokenAmount) external;
 
-    function getBalanceLocked(AssetToken assetToken) external view returns (uint256 balanceLocked);
-    function acceptYieldAllowance(AssetToken assetToken, uint256 amount, uint256 expiration) external;
+    function wallet() external view returns (address wallet);
+    function getBalanceLocked(IAssetToken assetToken) external view returns (uint256 balanceLocked);
+    function acceptYieldAllowance(IAssetToken assetToken, uint256 amount, uint256 expiration) external;
     function renounceYieldDistribution(
-        AssetToken assetToken,
+        IAssetToken assetToken,
         uint256 amount,
         uint256 expiration
     ) external returns (uint256 amountRenounced);
-    function clearYieldDistributions(AssetToken assetToken) external;
+    function clearYieldDistributions(IAssetToken assetToken) external;
 
 }
