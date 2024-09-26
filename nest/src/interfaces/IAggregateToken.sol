a29143bf0ce8c8267d31b333410c76fd9d5e7b21
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-09-25 19:19:53 -0700

    update interfaces

--- nest/src/interfaces/IAggregateToken.sol
+++ nest/src/interfaces/IAggregateToken.sol
@@ -5,8 +5,29 @@ import { IComponentToken } from "./IComponentToken.sol";
 
 interface IAggregateToken is IComponentToken {
 
+    /**
+     * @notice Buy ComponentToken using CurrencyToken
+     * @dev Only the owner can call this function, will revert if
+     *   the AggregateToken does not have enough CurrencyToken to buy the ComponentToken
+     * @param componentToken ComponentToken to buy
+     * @param currencyTokenAmount Amount of CurrencyToken to pay to receive the ComponentToken
+     */
     function buyComponentToken(IComponentToken componentToken, uint256 currencyTokenAmount) external;
+
+    /**
+     * @notice Sell ComponentToken to receive CurrencyToken
+     * @dev Only the owner can call this function, will revert if
+     *   the ComponentToken does not have enough CurrencyToken to sell to the AggregateToken
+     * @param componentToken ComponentToken to sell
+     * @param currencyTokenAmount Amount of CurrencyToken to receive in exchange for the ComponentToken
+     */
     function sellComponentToken(IComponentToken componentToken, uint256 currencyTokenAmount) external;
+
+    /**
+     * @notice Claim yield for all ComponentTokens into the AggregateToken
+     * @dev Anyone can call this function to claim yield for all ComponentTokens
+     */
     function claimComponentsYield() external returns (uint256 amount);
+    function requestYield(address from) external;
 
 }
--- nest/src/interfaces/IComponentToken.sol
+++ nest/src/interfaces/IComponentToken.sol
@@ -1,21 +1,76 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import { IComponentToken } from "./IComponentToken.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IComponentToken is IERC20 {
+interface IYieldDistributionToken is IERC20 {
+    /// @notice CurrencyToken in which yield is denominated and distributed
+    function getCurrencyToken() external returns (IERC20 currencyToken);
 
+    /**
+     * @notice Claim yield for the given user
+     * @dev Anyone can call this function to claim yield for any user
+     * @param user Address of the user for which to claim yield
+     * @return currencyToken CurrencyToken in which yield is denominated and distributed
+     * @return currencyTokenAmount Amount of yield claimed by the user
+     */
+    function claimYield(address user) external returns (IERC20 currencyToken, uint256 currencyTokenAmount);
+
+    function accrueYield(address user) external;
+    function requestYield(address from) external;
+}
+
+interface IComponentToken is IYieldDistributionToken {
+
+    /**
+     * @notice Buy FakeComponentToken using CurrencyToken
+     * @dev The user must approve the contract to spend the CurrencyToken
+     * @param currencyToken CurrencyToken used to buy the FakeComponentToken
+     * @param currencyTokenAmount Amount of CurrencyToken to pay to receive the same amount of FakeComponentToken
+     * @return componentTokenAmount Amount of FakeComponentToken received
+     */
     function buy(IERC20 currencyToken, uint256 currencyTokenAmount) external returns (uint256 componentTokenAmount);
-    function sell(IERC20 currencyToken, uint256 currencyTokenAmount) external returns (uint256 componentTokenAmount);
-    function claimYield(address user) external returns (uint256 amount);
 
+    /**
+     * @notice Sell FakeComponentToken to receive CurrencyToken
+     * @param currencyToken CurrencyToken received in exchange for the FakeComponentToken
+     * @param currencyTokenAmount Amount of CurrencyToken to receive in exchange for the FakeComponentToken
+     * @return componentTokenAmount Amount of FakeComponentToken sold
+     */
+    function sell(IERC20 currencyToken, uint256 currencyTokenAmount) external returns (uint256 componentTokenAmount);
+
+    /// @notice Version of the FakeComponentToken
     function getVersion() external view returns (uint256 version);
-    function getCurrencyToken() external view returns (IERC20 currencyToken);
-    function totalYield() external view returns (uint256 amount);
-    function claimedYield() external view returns (uint256 amount);
-    function unclaimedYield() external view returns (uint256 amount);
-    function totalYield(address user) external view returns (uint256 amount);
-    function claimedYield(address user) external view returns (uint256 amount);
-    function unclaimedYield(address user) external view returns (uint256 amount);
+
+    /// @notice Total yield distributed to all FakeComponentTokens for all users
+    function totalYield() external view returns (uint256 currencyTokenAmount);
+
+    /// @notice Claimed yield across all FakeComponentTokens for all users
+    function claimedYield() external view returns (uint256 currencyTokenAmount);
+
+    /// @notice Unclaimed yield across all FakeComponentTokens for all users
+    function unclaimedYield() external view returns (uint256 currencyTokenAmount);
+
+    /**
+     * @notice Total yield distributed to a specific user
+     * @param user Address of the user for which to get the total yield
+     * @return currencyTokenAmount Total yield distributed to the user
+     */
+    function totalYield(address user) external view returns (uint256 currencyTokenAmount);
+
+    /**
+     * @notice Amount of yield that a specific user has claimed
+     * @param user Address of the user for which to get the claimed yield
+     * @return currencyTokenAmount Amount of yield that the user has claimed
+     */
+    function claimedYield(address user) external view returns (uint256 currencyTokenAmount);
+
+    /**
+     * @notice Amount of yield that a specific user has not yet claimed
+     * @param user Address of the user for which to get the unclaimed yield
+     * @return currencyTokenAmount Amount of yield that the user has not yet claimed
+     */
+    function unclaimedYield(address user) external view returns (uint256 currencyTokenAmount);
 
 }
