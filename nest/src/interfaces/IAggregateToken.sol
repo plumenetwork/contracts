eb1269f49c004c64126aafa7f5d2d7724178d45b
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-09-27 08:30:43 -0700

    temp interfaces

--- nest/src/interfaces/IAggregateToken.sol
+++ nest/src/interfaces/IAggregateToken.sol
@@ -5,7 +5,40 @@ import { IComponentToken } from "./IComponentToken.sol";
 
 interface IAggregateToken is IComponentToken {
 
+    /// The vault that caontains this tells NEV I want to buy $NEV using $pUSD, they take the $pUSD and mint $NEV for me - all synchronous
+    function buy(IERC20 currencyToken, uint256 currencyTokenAmount) external returns (uint256 componentTokenAmount);
+
+    /// The vault that caontains this tells NEV I want to sell this much $NEV, they tell me they will give me this much $pUSD
+    function offer(IERC20 currencyToken, uint256 componentTokenAmount) external returns (uint256 currencyTokenAmount);
+
+    /// Cicada this will call componentToken.buy
     function buyComponentToken(IComponentToken componentToken, uint256 currencyTokenAmount) external;
-    function sellComponentToken(IComponentToken componentToken, uint256 currencyTokenAmount) external;
+
+    /// Cicada this will call componentToken.offer
+    function offerComponentToken(IComponentToken componentToken, uint256 componentTokenAmount) external;
+
+    /// Credbull tells NEV I have the $pUSD for you, they give NEV the $pUSD and burn the $CBL from NEV
+    // we can check that this is only being called by a componentToken
+    function sellComponentToken(IERC20 currencyToken, IERC20 componentToken, uint256 componentTokenAmount) external returns (uint256 currencyTokenAmount);
+
+    /// Credbull tells NEV I have $pUSD yield for you and gives it to NEV
+    function depositYield(IERC20 currencyToken, uint256 currencyTokenAmount) external;
+
+
+
+
+
+
+
+
+    function claimYield(address user) external returns (uint256 currencyTokenAmount);
+    /// NEV tells each user how much unclaimed yield there is
+    function totalYield() external view returns (uint256 amount);
+    function claimedYield() external view returns (uint256 amount);
+    function unclaimedYield() external view returns (uint256 amount);
+    function totalYield(address user) external view returns (uint256 amount);
+    function claimedYield(address user) external view returns (uint256 amount);
+    function unclaimedYield(address user) external view returns (uint256 amount);
+}
 
 }
--- nest/src/interfaces/IComponentToken.sol
+++ nest/src/interfaces/IComponentToken.sol
@@ -3,19 +3,45 @@ pragma solidity ^0.8.25;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-interface IComponentToken is IERC20 {
-
-    function buy(IERC20 currencyToken, uint256 currencyTokenAmount) external returns (uint256 componentTokenAmount);
-    function sell(IERC20 currencyToken, uint256 currencyTokenAmount) external returns (uint256 componentTokenAmount);
-    function claimYield(address user) external returns (uint256 amount);
-
-    function getVersion() external view returns (uint256 version);
-    function getCurrencyToken() external view returns (IERC20 currencyToken);
+interface IClaimableYieldToken {
+    /// this token has a whitelist and only whitelisted people can call buy and offer
+    /// NEV tells Credbull please give me the yield you told me i had, in $pUSD
+    function claimYield(address user) external returns (uint256 currencyTokenAmount);
     function totalYield() external view returns (uint256 amount);
     function claimedYield() external view returns (uint256 amount);
     function unclaimedYield() external view returns (uint256 amount);
     function totalYield(address user) external view returns (uint256 amount);
     function claimedYield(address user) external view returns (uint256 amount);
     function unclaimedYield(address user) external view returns (uint256 amount);
+}
 
+interface IComponentToken {
+
+    /// NEV tells Credbull I want to buy $CBL using $pUSD, they take the $pUSD and mint $CBL for me - all synchronous
+    function buy(IERC20 currencyToken, uint256 currencyTokenAmount) external returns (uint256 componentTokenAmount);
+
+    /// NEV tells Credbull I want to sell this much $CBL, they tell me they will give me this much $pUSD
+    function offer(IERC20 currencyToken, uint256 componentTokenAmount) external returns (uint256 currencyTokenAmount);
+}
+
+interface IAggregateToken is IComponentToken {
+
+    /// The vault that caontains this tells NEV I want to buy $NEV using $pUSD, they take the $pUSD and mint $NEV for me - all synchronous
+    function buy(IERC20 currencyToken, uint256 currencyTokenAmount) external returns (uint256 componentTokenAmount);
+
+    /// The vault that caontains this tells NEV I want to buy $NEV using $pUSD, they take the $pUSD and mint $NEV for me - all synchronous
+    function sell(IERC20 currencyToken, uint256 currencyTokenAmount) external returns (uint256 componentTokenAmount);
+
+    /// Cicada this will call componentToken.buy
+    function buyComponentToken(IComponentToken componentToken, uint256 currencyTokenAmount) external;
+
+    /// Cicada this will call componentToken.offer
+    function offerComponentToken(IComponentToken componentToken, uint256 componentTokenAmount) external;
+
+    /// Credbull tells NEV I have the $pUSD for you, they give NEV the $pUSD and burn the $CBL from NEV
+    // we can check that this is only being called by a componentToken
+    function sellComponentToken(IERC20 currencyToken, IERC20 componentToken, uint256 componentTokenAmount) external returns (uint256 currencyTokenAmount);
+
+    /// Credbull tells NEV I have $pUSD yield for you and gives it to NEV
+    function depositYield(IERC20 currencyToken, uint256 currencyTokenAmount) external;
 }
