1cdeec18b57cb6d9aa21aeb019c2fd2d37dffdb7
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-10-02 11:05:10 -0700

    add test for WalletFactory

---
+++ smart-wallets/test/WalletFactory.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Test } from "forge-std/Test.sol";
+
+import { SmartWallet } from "../src/SmartWallet.sol";
+import { WalletFactory } from "../src/WalletFactory.sol";
+import { ISmartWallet } from "../src/interfaces/ISmartWallet.sol";
+
+contract WalletFactoryTest is Test {
+
+    SmartWallet smartWallet;
+    WalletFactory walletFactory;
+
+    address owner = address(0x1234);
+    address user1 = address(0xBEEF);
+
+    function setUp() public {
+        smartWallet = new SmartWallet();
+        walletFactory = new WalletFactory(owner, smartWallet);
+    }
+
+    function test_constructor() public {
+        WalletFactory walletFactoryConstructor = new WalletFactory(owner, smartWallet);
+        assertEq(walletFactoryConstructor.owner(), owner);
+        assertEq(address(walletFactoryConstructor.smartWallet()), address(smartWallet));
+    }
+
+    function test_upgradeFail() public {
+        ISmartWallet newImplementation = new SmartWallet();
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user1));
+        vm.startPrank(user1);
+        walletFactory.upgrade(newImplementation);
+        vm.stopPrank();
+    }
+
+    function test_upgrade() public {
+        ISmartWallet newImplementation = new SmartWallet();
+        vm.startPrank(owner);
+        walletFactory.upgrade(newImplementation);
+        vm.stopPrank();
+
+        assertEq(walletFactory.owner(), owner);
+        assertEq(address(walletFactory.smartWallet()), address(newImplementation));
+    }
+
+}
