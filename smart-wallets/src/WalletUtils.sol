37ccf1b8298bb4d5d264ddd918bbe71404ccfe05
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-10-06 11:34:55 -0700

    [CHAIN-56] only call SmartWallet with low-level calls (#43)

--- smart-wallets/src/WalletUtils.sol
+++ smart-wallets/src/WalletUtils.sol
@@ -9,6 +9,12 @@ pragma solidity ^0.8.25;
 contract WalletUtils {
 
     /**
+     * @notice Indicates a failure because the user's SmartWallet call failed
+     * @param user Address of the user whose SmartWallet call failed
+     */
+    error SmartWalletCallFailed(address user);
+
+    /**
      * @notice Indicates a failure because the caller is not the user wallet
      * @param invalidUser Address of the caller who tried to call a wallet-only function
      */
@@ -20,21 +26,6 @@ contract WalletUtils {
             revert UnauthorizedCall(msg.sender);
         }
         _;
-    }
-
-    /**
-     * @notice Checks if an address is a contract or smart wallet.
-     * @dev This function uses the `extcodesize` opcode to check if the target address contains contract code.
-     * It returns true for contracts and smart wallets, and false for EOAs that do not have smart wallets.
-     * @param addr Address to check
-     * @return hasCode True if the address is a contract or smart wallet, and false if it is not
-     */
-    function isContract(address addr) internal view returns (bool hasCode) {
-        uint32 size;
-        assembly {
-            size := extcodesize(addr)
-        }
-        return size > 0;
     }
 
 }
