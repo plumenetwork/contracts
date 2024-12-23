5d8cb32e309e55aaaf0fde3269c303cd6296c210
ungaro <alp.guneysel@gmail.com>
2024-12-23 14:40:42 -0500

    NestBoringVaultModule init

--- nest/src/vault/NestTeller.sol
+++ nest/src/vault/NestTeller.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.25;
 
 import { MultiChainLayerZeroTellerWithMultiAssetSupport } from "@nucleus-boring-vault/base/Roles/MultiChainLayerZeroTellerWithMultiAssetSupport.sol";
 import { FixedPointMathLib } from "@solmate/utils/FixedPointMathLib.sol";
+import { NestBoringVaultModule } from "./NestBoringVaultModule.sol";
 
 /**
  * @title NestTeller
@@ -10,60 +11,7 @@ import { FixedPointMathLib } from "@solmate/utils/FixedPointMathLib.sol";
  * @dev A Teller that only allows deposits of a single `asset` that is
  * configured.
  */
-contract NestTeller is MultiChainLayerZeroTellerWithMultiAssetSupport {
-
-    // Libraries
-
-    using FixedPointMathLib for uint256;
-
-    // Public State
-
-    address public asset;
-    uint256 public minimumMintPercentage; // Must be 4 decimals i.e. 9999 = 99.99%
-
-    // Errors
-
-    error Unimplemented();
-
-    // Constructor
-
-    constructor(
-        address _owner,
-        address _vault,
-        address _accountant,
-        address _endpoint,
-        address _asset,
-        uint256 _minimumMintPercentage
-    ) MultiChainLayerZeroTellerWithMultiAssetSupport(
-        _owner,
-        _vault,
-        _accountant,
-        _endpoint
-    ) {
-        asset = _asset;
-        minimumMintPercentage = _minimumMintPercentage;
-    }
-
-    // Admin Setters
-
-    function setAsset(address _asset) requiresAuth external {
-        asset = _asset;
-    }
-
-    function setMinimumMintPercentage(uint256 _minimumMintPercentage) requiresAuth external {
-        minimumMintPercentage = _minimumMintPercentage;
-    }
-
-    /**
-     * @notice Transfer assets from the owner into the vault and submit a request to buy shares
-     * @param assets Amount of `asset` to deposit
-     * @param controller Controller of the request
-     * @param owner Source of the assets to deposit
-     * @return requestId Discriminator between non-fungible requests
-     */
-    function requestDeposit(uint256 assets, address controller, address owner) public returns (uint256 requestId) {
-        revert Unimplemented();
-    }
+contract NestTeller is NestBoringVaultModule,MultiChainLayerZeroTellerWithMultiAssetSupport {
 
     /**
      * @notice Fulfill a request to buy shares by minting shares to the receiver
