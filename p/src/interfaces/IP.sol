0fcc4403139e092dfeb21af5a7b123b19946bde3
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2024-12-30 13:25:29 -0500

    [PDE-438] rename $P to $PLUME

--- p/src/interfaces/IP.sol
+++ p/src/interfaces/IP.sol
@@ -5,7 +5,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { IERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
 
-interface IP is IERC20, IERC20Metadata, IERC20Permit {
+interface IPlume is IERC20, IERC20Metadata, IERC20Permit {
 
     function mint(address to, uint256 amount) external;
     function burn(address from, uint256 amount) external;
--- p/src/proxy/PlumeProxy.sol
+++ p/src/proxy/PlumeProxy.sol
@@ -4,17 +4,17 @@ pragma solidity ^0.8.25;
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 /**
- * @title PProxy
+ * @title PlumeProxy
  * @author Eugene Y. Q. Shen
- * @notice Proxy contract for P
+ * @notice Proxy contract for Plume
  */
-contract PProxy is ERC1967Proxy {
+contract PlumeProxy is ERC1967Proxy {
 
     /// @notice Indicates a failure because transferring ETH to the proxy is unsupported
     error ETHTransferUnsupported();
 
     /// @notice Name of the proxy, used to ensure each named proxy has unique bytecode
-    bytes32 public constant PROXY_NAME = keccak256("PProxy");
+    bytes32 public constant PROXY_NAME = keccak256("PlumeProxy");
 
     constructor(address logic, bytes memory data) ERC1967Proxy(logic, data) { }
 
