10546fd0973cce1740de798ccd450f3005fda47d
hidewrong <hidewrong@outlook.com>
2025-01-15 00:35:22 +0800

    chore: fix typo in comment (#154)

--- nest/src/interfaces/IERC7575.sol
+++ nest/src/interfaces/IERC7575.sol
@@ -6,7 +6,7 @@ import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 interface IERC7575 is IERC165, IERC4626 {
 
-    /// @notice Address of the underlying `share` receivd on deposit into the vault
+    /// @notice Address of the underlying `share` received on deposit into the vault
     function share() external view returns (address shareTokenAddress);
 
 }
