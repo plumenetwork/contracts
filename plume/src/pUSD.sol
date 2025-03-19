716eacdc49aa511e2593afacf82b667d42073ae8
Eugene Y. Q. Shen <eyqs@plumenetwork.xyz>
2025-03-19 17:46:25 -0400

    add contract for testnet pUSD

---
+++ plume/src/pUSD.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract pUSD is ERC20 {
+    constructor() ERC20("Plume USD", "pUSD") {
+        _mint(msg.sender, 1_000_000_000 * 10 ** decimals());
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+}
