2b50d70e5996334a083536521a16385fecd99a67
Purva-Chaudhari <purva.chaudhari02@gmail.com>
2025-04-14 12:53:57 -0400

    name

--- plume/src/proxy/RaffleProxy.sol
+++ plume/src/proxy/RaffleProxy.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.25;
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 
-contract RaffleInternalProxy is ERC1967Proxy {
+contract RaffleProxy is ERC1967Proxy {
 
     /// @notice Indicates a failure because transferring ETH to the proxy is unsupported
     error ETHTransferUnsupported();
