4e782bf3824ebc7bce4aeaccaff75e6ff33ba147
ungaro <alp.guneysel@gmail.com>
2025-04-15 14:30:04 -0400

    update solidity version

--- p/src/facets/AccessControlFacet.sol
+++ p/src/facets/AccessControlFacet.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.25;
 
 // SolidState Access Control
 import { AccessControlInternal } from "@solidstate/access/access_control/AccessControlInternal.sol";
