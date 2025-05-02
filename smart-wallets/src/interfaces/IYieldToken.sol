e84b38b6f78bf49ca469164971ab24df877bef27
ungaro <alp.guneysel@gmail.com>
2025-05-02 18:02:23 -0400

    Remove smart-wallets from PR

--- smart-wallets/src/interfaces/IYieldToken.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
-
-import { IYieldDistributionToken } from "./IYieldDistributionToken.sol";
-import { IYieldReceiver } from "./IYieldReceiver.sol";
-
-interface IYieldToken is IYieldDistributionToken, IYieldReceiver { }
--- smart-wallets/src/mocks/MockInvalidSmartWallet.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
-
-contract MockInvalidSmartWallet {
-// Empty contract that will fail when called
-}
