209ab9476bdb97e3174c56c3219c71704dbd4c48
ravinagill15 <ravina@layerzerolabs.org>
2025-07-15 12:22:48 -0700

    contracts, deploy scripts, tasks

---
+++ plume-oft-adapter-migration/deploy/OrbitERC20OFTAdapterUpgradeable.ts
@@ -0,0 +1,39 @@
+import { type DeployFunction } from 'hardhat-deploy/types'
+
+import { EndpointId, endpointIdToNetwork } from '@layerzerolabs/lz-definitions'
+import { getDeploymentAddressAndAbi } from '@layerzerolabs/lz-evm-sdk-v2'
+
+const contractName = 'OrbitERC20OFTAdapterUpgradeable'
+
+const deploy: DeployFunction = async (hre) => {
+    const { deploy } = hre.deployments
+    const signer = (await hre.ethers.getSigners())[0]
+    console.log(`deploying ${contractName} on network: ${hre.network.name} with ${signer.address}`)
+
+    const eid = hre.network.config.eid as EndpointId
+    const lzNetworkName = endpointIdToNetwork(eid)
+
+    const { address } = getDeploymentAddressAndAbi(lzNetworkName, 'EndpointV2')
+
+    await deploy(contractName, {
+        from: signer.address,
+        args: ['0x4C1746A800D224393fE2470C70A35717eD4eA5F1', address, '0x35381f63091926750F43b2A7401B083263aDEF83'], // replace '0xtoken' with the address of the ERC-20 token and '0xbridge' with the address of the bridge
+        log: true,
+        waitConfirmations: 1,
+        skipIfAlreadyDeployed: false,
+        proxy: {
+            proxyContract: 'OpenZeppelinTransparentProxy',
+            owner: signer.address,
+            execute: {
+                init: {
+                    methodName: 'initialize',
+                    args: [signer.address],
+                },
+            },
+        },
+    })
+}
+
+deploy.tags = [contractName]
+
+export default deploy
---
+++ plume-oft-adapter-migration/layerzero.config.ts
@@ -0,0 +1,69 @@
+import { EndpointId } from '@layerzerolabs/lz-definitions'
+import { ExecutorOptionType } from '@layerzerolabs/lz-v2-utilities'
+import { TwoWayConfig, generateConnectionsConfig } from '@layerzerolabs/metadata-tools'
+import { OAppEnforcedOption } from '@layerzerolabs/toolbox-hardhat'
+
+import type { OmniPointHardhat } from '@layerzerolabs/toolbox-hardhat'
+
+const ethereumContract: OmniPointHardhat = {
+    eid: EndpointId.ETHEREUM_V2_MAINNET,
+    contractName: 'OrbitERC20OFTAdapterUpgradeable',
+}
+
+const plumeContract: OmniPointHardhat = {
+    eid: EndpointId.PLUMEPHOENIX_V2_MAINNET,
+    contractName: 'OrbitNativeOFTAdapter',
+}
+
+const existingEthereumPlumeOFTAdapterContract: OmniPointHardhat = {
+    eid: EndpointId.ETHEREUM_V2_MAINNET,
+    address: '0xbDA8a2285F4C3e75b37E467C4DB9bC633FfbD29d',
+}
+
+const plumeDummyContract: OmniPointHardhat = {
+    eid: EndpointId.PLUMEPHOENIX_V2_MAINNET,
+    contractName: 'PlumeOFTMock',
+}
+
+// To connect all the above chains to each other, we need the following pathways:
+// Optimism <-> Arbitrum
+
+// For this example's simplicity, we will use the same enforced options values for sending to all chains
+// For production, you should ensure `gas` is set to the correct value through profiling the gas usage of calling OFT._lzReceive(...) on the destination chain
+// To learn more, read https://docs.layerzero.network/v2/concepts/applications/oapp-standard#execution-options-and-enforced-settings
+const EVM_ENFORCED_OPTIONS: OAppEnforcedOption[] = [
+    {
+        msgType: 1,
+        optionType: ExecutorOptionType.LZ_RECEIVE,
+        gas: 80000,
+        value: 0,
+    },
+]
+// With the config generator, pathways declared are automatically bidirectional
+// i.e. if you declare A,B there's no need to declare B,A
+const pathways: TwoWayConfig[] = [
+    [
+        ethereumContract, // Chain A contract
+        plumeContract, // Chain C contract
+        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ] // TODO update dvns
+        [1, 1], // [A to B confirmations, B to A confirmations] // TODO update confirmations
+        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain C enforcedOptions, Chain A enforcedOptions // TODO confirm this
+    ],
+    [
+        // TODO this cannot be done by LZ -- Plume owns existing PlumeOFTAdapter contract on Ethereum
+        existingEthereumPlumeOFTAdapterContract, // Chain A contract
+        plumeDummyContract, // Chain C contract
+        [['LayerZero Labs'], []], // [ requiredDVN[], [ optionalDVN[], threshold ] ] // TODO update dvns
+        [1, 1], // [A to B confirmations, B to A confirmations] // TODO update confirmations
+        [EVM_ENFORCED_OPTIONS, EVM_ENFORCED_OPTIONS], // Chain C enforcedOptions, Chain A enforcedOptions // TODO confirm this
+    ],
+]
+
+export default async function () {
+    // Generate the connections config based on the pathways
+    const connections = await generateConnectionsConfig(pathways)
+    return {
+        contracts: [{ contract: ethereumContract }, { contract: plumeContract }],
+        connections,
+    }
+}
